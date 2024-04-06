fadbc698a1c7c43255bb7912c889fe4c99252e5e
washington-shoji <shoji.washington@gmail.com>
2024-04-06 20:45:06 +1100

    -adding default testing db configs

--- src/database/db.ts
+++ src/database/db.ts
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-	user: process.env.DB_USER,
-	database: process.env.DB_NAME,
-	port: parseInt(process.env.DB_PORT as string),
-	host: process.env.DB_HOST,
-	password: process.env.DB_PASSWORD,
+	user: process.env.DB_USER || 'testuser',
+	database: process.env.DB_NAME || 'testdb',
+	port: parseInt(process.env.DB_PORT as string) || 5432,
+	host: process.env.DB_HOST || 'localhost',
+	password: process.env.DB_PASSWORD || 'testpass',
 	ssl:
 		process.env.NODE_ENV === 'production'
 			? { rejectUnauthorized: false }
