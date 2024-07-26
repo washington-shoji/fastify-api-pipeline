7eb0d0fea01b676a12dff33de7bc7ca3978e0a28
washington-shoji <shoji.washington@gmail.com>
2024-07-27 09:28:37 +1000

    - updated event registered controller

--- src/controllers/event-registered.controller.ts
+++ src/controllers/event-registered.controller.ts
@@ -8,10 +8,9 @@ export async function getAllRegisteredEventsController(
 ) {
 	try {
 		const token = request.headers.authorization?.split(' ')[1]; // Bearer TOKEN
-		console.log('token >>>>>>>>>>>>>>>>', token);
 		const decoded = decodeToken(token);
 		const userId = decoded?.userId;
-		console.log('userId >>>>>>>>>>>>>>>>', userId);
+
 		if (!userId) {
 			return reply.code(401).send({ message: 'Unauthorized' });
 		}
