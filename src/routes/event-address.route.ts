e8910123ccea8ac39dbb0ee39e71a3021e815e52
washington-shoji <shoji.washington@gmail.com>
2024-09-04 22:53:49 +1000

    - added resquest schema to event address route

--- src/routes/event-address.route.ts
+++ src/routes/event-address.route.ts
@@ -9,26 +9,58 @@ import {
 } from '../controllers/event-address.controller';
 import { authMiddleware } from '../middlewares/auth.middleware';
 
+
+const bodyJsonSchema = {
+	type: 'object',
+	required: ['street', 'city_suburb', 'state', 'country', 'postal_code'],
+	properties: {
+		address_id: { type: 'string' },
+		street: { type: 'string' },
+		city_suburb: { type: 'string' },
+		state: { type: 'string' },
+		country: { type: 'string' },
+		postal_code: { type: 'string' },
+	}
+  }
+
+  const paramsJsonSchema = {
+	type: 'object',
+	properties: {
+		id: { type: 'string' },
+		eventId: { type: 'string' },
+	}
+  }
+
+  const schema = {
+	body: bodyJsonSchema,
+	params: paramsJsonSchema,
+  }
+
+
 export default async function eventAddressRoutes(
 	fastify: FastifyInstance
 ): Promise<void> {
 	fastify.addHook('preHandler', authMiddleware);
-	fastify.post('/events-address/event/:eventId', createEventAddressController);
-	fastify.get('/events-address/events/:eventId', getEventsAddressesController);
+	fastify.post('/events-address/event/:eventId', {schema}, createEventAddressController);
+	fastify.get('/events-address/events/:eventId', {schema}, getEventsAddressesController);
 	fastify.get(
 		'/events-address/:id/event/:eventId',
+		{schema},
 		findEventAddressByIdController
 	);
 	fastify.get(
 		'/events-address/event/:eventId',
+		{schema}, 
 		findEventAddressByEventIdController
 	);
 	fastify.put(
-		'/events-address/:id/event/:eventId',
+		'/events-address/:id/event/:eventId', 
+		{schema}, 
 		updateEventAddressController
 	);
 	fastify.delete(
 		'/events-address/:id/event/:eventId',
+		{schema},
 		deleteEventAddressController
 	);
 }
