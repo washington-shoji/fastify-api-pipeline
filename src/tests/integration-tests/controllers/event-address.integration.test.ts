4c7cb2e6b85e6d79b94a0df86638070484738985
washington-shoji <shoji.washington@gmail.com>
2024-09-09 17:05:33 +1000

    - updated event address integration test

--- src/tests/integration-tests/controllers/event-address.integration.test.ts
+++ src/tests/integration-tests/controllers/event-address.integration.test.ts
@@ -1,94 +1,232 @@
-import fastify, { FastifyInstance } from 'fastify';
-import { authMiddleware } from '../../../middlewares/auth.middleware';
-import { EventAddressModelRequest } from '../../../models/event-address-model';
-import { createEventAddressController } from '../../../controllers/event-address.controller';
-import { registerUserService, testDeleteUserService } from '../../../services/auth.service';
-import { testToken } from '../../unit-tests/utils/test-jwt-token';
-import { deleteEventAddressService } from '../../../services/event-address.service';
-import { EventResponseModel, LOCATION_TYPE } from '../../../models/event-model';
-import { createEventService, deleteEventService } from '../../../services/event.service';
+import fastify, { FastifyInstance } from "fastify";
+import { authMiddleware } from "../../../middlewares/auth.middleware";
+import {
+  EventAddressModelRequest,
+  EventAddressModelResponse,
+} from "../../../models/event-address-model";
+import {
+  createEventAddressController,
+  updateEventAddressController,
+} from "../../../controllers/event-address.controller";
+import {
+  registerUserService,
+  testDeleteUserService,
+} from "../../../services/auth.service";
+import { testToken } from "../../unit-tests/utils/test-jwt-token";
+import {
+  createEventAddressService,
+  deleteEventAddressService,
+} from "../../../services/event-address.service";
+import { EventResponseModel, LOCATION_TYPE } from "../../../models/event-model";
+import {
+  createEventService,
+  deleteEventService,
+} from "../../../services/event.service";
 
+describe("Event Address Controllers - createEventAddressController", () => {
+  let app: FastifyInstance;
+  let userId: string;
+  let eventId: string;
+  let createdEventAddressId: string;
+  let testEvent: EventResponseModel;
+  let token: string;
 
+  beforeEach(async () => {
+    app = fastify();
+    app.addHook("preHandler", authMiddleware);
+    app.post<{ Params: { eventId: string }; Body: EventAddressModelRequest }>(
+      "/events-address/event/:eventId",
+      createEventAddressController
+    );
 
-describe('Event Address Controllers - createEventAddressController', () => {
-	let app: FastifyInstance;
-	let userId: string;
-    let eventId: string;
-	let createdEventAddressId: string;
-    let testEvent: EventResponseModel;
-	let token: string;
+    const result = await registerUserService(
+      `${Math.random().toString()}`,
+      `${Math.random().toString()}@email.com`,
+      "test-pass-disabled"
+    );
 
-    beforeEach(async () => {
-		app = fastify();
-		app.addHook('preHandler', authMiddleware);
-		app.post<{Params: { eventId: string }; Body: EventAddressModelRequest; }>('/events-address/event/:eventId', createEventAddressController);
+    userId = result.user_id;
+    token = testToken(userId);
 
-		const result = await registerUserService(
-			`${Math.random().toString()}`,
-			`${Math.random().toString()}@email.com`,
-			'test-pass-disabled'
-		);
+    const eventData = {
+      title: "TEST Event to Update",
+      description: "This event will be updated in the test",
+      registration_open: new Date(),
+      registration_close: new Date(),
+      event_date: new Date(),
+      location_type: <LOCATION_TYPE>"ONLINE",
+    };
 
-		userId = result.user_id;
-		token = testToken(userId);
+    // Create a test event to fetch later
+    testEvent = await createEventService(userId, eventData);
+    eventId = testEvent.event_id;
+  });
 
-        const eventData = {
-			title: 'TEST Event to Update',
-			description: 'This event will be updated in the test',
-			registration_open: new Date(),
-			registration_close: new Date(),
-			event_date: new Date(),
-			location_type: <LOCATION_TYPE>'ONLINE',
-		};
+  afterEach(async () => {
+    // Teardown: Delete all created events to clean up the database
+    await deleteEventService(testEvent.event_id, userId);
+    await deleteEventAddressService(createdEventAddressId, eventId);
+    await testDeleteUserService(userId);
+    await app.close();
+  });
 
-		// Create a test event to fetch later
-		testEvent = await createEventService(userId, eventData);
-        eventId = testEvent.event_id;
-        
-	});
+  it("should create an event address and return 201 status", async () => {
+    const testEventAddressRequestData = {
+      street: "1 Sydney St",
+      city_suburb: "Sydney",
+      state: "NSW",
+      country: "Australia",
+      postal_code: "2000",
+    };
 
-    afterEach(async () => {
-		// Teardown: Delete all created events to clean up the database
-        await deleteEventService(testEvent.event_id, userId);
-		await deleteEventAddressService(createdEventAddressId, eventId);
-		await testDeleteUserService(userId);
-		await app.close();
-	});
-
-
-    it('should create an event address and return 201 status', async () => {
-
-        const testEventAddressRequestData = {
-            street: '1 Sydney St',
-            city_suburb: 'Sydney',
-            state: 'NSW',
-            country: 'Australia',
-            postal_code: '2000'
-        };
-
-        const response = await app.inject({
-			method: 'POST',
-			url: `/events-address/event/${eventId}`,
-			headers: {
-				authorization: `Bearer ${token}`,
-			},
-			payload: testEventAddressRequestData,
-		});
-
-        expect(response.statusCode).toBe(201);
-		const createdAddressEvent = JSON.parse(response.body);
-		createdEventAddressId = createdAddressEvent.address_id;
-
-        const testEventAddressResponseData = {
-            address_id: createdAddressEvent.address_id,
-            street: '1 Sydney St',
-            city_suburb: 'Sydney',
-            state: 'NSW',
-            country: 'Australia',
-            postal_code: '2000'
-        };
-
-        expect(createdAddressEvent).toStrictEqual(testEventAddressResponseData);
+    const response = await app.inject({
+      method: "POST",
+      url: `/events-address/event/${eventId}`,
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+      payload: testEventAddressRequestData,
     });
 
+    expect(response.statusCode).toBe(201);
+    const createdAddressEvent = JSON.parse(response.body);
+    createdEventAddressId = createdAddressEvent.address_id;
+
+    const testEventAddressResponseData = {
+      address_id: createdAddressEvent.address_id,
+      street: "1 Sydney St",
+      city_suburb: "Sydney",
+      state: "NSW",
+      country: "Australia",
+      postal_code: "2000",
+    };
+
+    expect(createdAddressEvent).toStrictEqual(testEventAddressResponseData);
+  });
+
+  it("should return 500 status on service failure", async () => {
+    const response = await app.inject({
+      method: "POST",
+      url: `/events-address/event/${eventId}`,
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+      payload: {}, // Incomplete or invalid event data to trigger error
+    });
+
+    expect(response.statusCode).toBe(500);
+  });
+});
+
+describe("Event Address Controllers - updateEventAddressController", () => {
+  let app: FastifyInstance;
+  let userId: string;
+  let eventId: string;
+  let createdEventAddressId: string;
+  let testEvent: EventResponseModel;
+  let testAddressEvent: EventAddressModelResponse;
+  let token: string;
+
+  beforeEach(async () => {
+    app = fastify();
+    app.addHook("preHandler", authMiddleware);
+    app.put<{
+      Params: { id: string; eventId: string };
+      Body: EventAddressModelRequest;
+    }>("/events-address/:id/event/:eventId", updateEventAddressController);
+
+    const result = await registerUserService(
+      `${Math.random().toString()}`,
+      `${Math.random().toString()}@email.com`,
+      "test-pass-disabled"
+    );
+
+    userId = result.user_id;
+    token = testToken(userId);
+
+    const eventData = {
+      title: "TEST Event to Update",
+      description: "This event will be updated in the test",
+      registration_open: new Date(),
+      registration_close: new Date(),
+      event_date: new Date(),
+      location_type: <LOCATION_TYPE>"ONLINE",
+    };
+
+    const eventAddressData = {
+      street: "1 Sydney St",
+      city_suburb: "Sydney",
+      state: "NSW",
+      country: "Australia",
+      postal_code: "2000",
+    };
+
+    // Create a test event to fetch later
+    testEvent = await createEventService(userId, eventData);
+    eventId = testEvent.event_id;
+
+    testAddressEvent = await createEventAddressService(
+      eventId,
+      eventAddressData
+    );
+
+    createdEventAddressId = testAddressEvent.address_id;
+  });
+
+  afterEach(async () => {
+    // Teardown: Delete all created events to clean up the database
+    await deleteEventService(testEvent.event_id, userId);
+    await deleteEventAddressService(createdEventAddressId, eventId);
+    await testDeleteUserService(userId);
+    await app.close();
+  });
+
+  it("should update an event address and return 200 status", async () => {
+    const updateEventAddressRequestData = {
+      street: "1 Melbourne St",
+      city_suburb: "Melbourne",
+      state: "VIC",
+      country: "Australia",
+      postal_code: "3000",
+    };
+
+    const response = await app.inject({
+      method: "PUT",
+      url: `/events-address/${createdEventAddressId}/event/${eventId}`,
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+      payload: updateEventAddressRequestData,
+    });
+
+    expect(response.statusCode).toBe(200);
+
+    const updatedEventAddress = JSON.parse(response.body);
+    expect(updatedEventAddress.street).toBe(
+      updateEventAddressRequestData.street
+    );
+    expect(updatedEventAddress.city_suburb).toBe(
+      updateEventAddressRequestData.city_suburb
+    );
+    expect(updatedEventAddress.state).toBe(updateEventAddressRequestData.state);
+    expect(updatedEventAddress.country).toBe(
+      updateEventAddressRequestData.country
+    );
+    expect(updatedEventAddress.postal_code).toBe(
+      updateEventAddressRequestData.postal_code
+    );
+  });
+
+  it("should return 500 status on service failure", async () => {
+    const response = await app.inject({
+      method: "PUT",
+      url: `/events-address/non-existent-id/event/${eventId}`,
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+      payload: {},
+    });
+
+    expect(response.statusCode).toBe(500);
+  });
 });
