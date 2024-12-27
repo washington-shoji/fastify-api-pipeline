223fbaa17f3c11233d34fc093020de9fbd0fbe76
washington-shoji <shoji.washington@gmail.com>
2024-12-28 08:40:13 +1100

    - updated event image model

--- src/models/event-image-model.ts
+++ src/models/event-image-model.ts
@@ -2,7 +2,16 @@ export interface EventImageModel {
 	id?: string;
 	eventId: string;
 	imageUrl: string;
-	imageKey: string;
+	imageKey?: string;
 	created_at?: Date;
 	updated_at?: Date;
+}
+
+export interface EventImageResponseModel {
+	id?: string;
+	imageUrl: string;
+}
+
+export interface EventPreSignedImageResponseModel {
+	imageUrl: string;
 }
